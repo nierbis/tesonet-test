2c52fbc820dbe8c40b708050f7a811507ccb6f3a
Arturas <zubearturas@gmail.com>
2019-10-11 13:01:47 +0300

    Add files via upload

---
+++ Tesonet-part3.playground/Contents.swift
@@ -0,0 +1,35 @@
+
+//func numeriai() -> Range<Int> {
+//
+//
+//    return 1 ..< 11
+//}
+//numeriai()
+
+
+//var n: Int
+//var y: Int
+
+let range = 1...10
+
+print(range.startIndex)
+print(range.endIndex)
+
+// Count returns the number of elements in the range.
+print(range.count)
+
+
+//
+//func print() {
+//
+//    var x = Int()
+//    var y = Int()
+//
+//    x.distance(to: y)
+//
+//
+//
+//}
+//
+//print(1,5)
+//
